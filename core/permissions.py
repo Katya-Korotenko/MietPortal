981c6e24cc8998b7da891e1ba7fc0b6e5b79bf16
Katya-Korotenko <korotenkok99943@gmail.com>
2026-09-09 20:49:25 +0200

    add base permissions

---
+++ core/permissions.py
@@ -0,0 +1,19 @@
+# core/permissions.py
+from rest_framework import permissions
+
+
+class IsLandlord(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated and request.user.is_landlord)
+
+
+class IsTenant(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated and request.user.is_tenant)
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.landlord == request.user
