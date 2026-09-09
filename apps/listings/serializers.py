8fcf63a53926ffffe2cbe8524a07ef0185897d7c
Katya-Korotenko <korotenkok99943@gmail.com>
2026-09-09 20:53:22 +0200

    add ListingSerializer

---
+++ apps/listings/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from core.constants import DEFAULT_CURRENCY
+from .models import Listing
+
+
+class ListingSerializer(serializers.ModelSerializer):
+    price_display = serializers.SerializerMethodField()
+    landlord = serializers.ReadOnlyField(source='landlord.username')
+
+    class Meta:
+        model = Listing
+        fields = (
+            'id', 'landlord', 'title', 'description', 'city', 'district',
+            'price', 'price_display', 'rooms', 'property_type', 'is_active',
+            'created_at', 'updated_at',
+        )
+        read_only_fields = ('id', 'landlord', 'created_at', 'updated_at')
+
+    def get_price_display(self, obj):
+        return f'{obj.price} {DEFAULT_CURRENCY}'
