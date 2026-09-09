f9af712d23f2c6b14d94a5c37811131e1d019dcc
Katya-Korotenko <korotenkok99943@gmail.com>
2026-09-09 20:24:49 +0200

    UPD User model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,7 +1,24 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from core.models import UniqueID
+from core.constants import LANDLORD_GROUP, TENANT_GROUP
 
 
-class User(AbstractUser):
-    pass
+class UserBasic(UniqueID, AbstractUser):
+    phone_number = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(unique=True)
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
 
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_landlord(self):
+        return self.groups.filter(name=LANDLORD_GROUP).exists()
+
+    @property
+    def is_tenant(self):
+        return self.groups.filter(name=TENANT_GROUP).exists()
