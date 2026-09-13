6f992e16dc4ee740467b726f3e16e21649286ad9
Katya-Korotenko <korotenkok99943@gmail.com>
2026-09-13 12:07:22 +0200

    UPD

--- apps/statistic/apps.py
+++ apps/statistic/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class StatisticConfig(AppConfig):
-    name = 'apps.statistics'
+    name = 'apps.statistic'
--- core/constants.py
+++ core/constants.py
@@ -2,3 +2,4 @@ DEFAULT_CURRENCY = 'EUR'
 TENANT_GROUP = 'Tenant'
 LANDLORD_GROUP = 'Landlord'
 DATE_INPUT_FORMATS = ['%Y-%m-%d', '%d.%m.%Y', '%d/%m/%Y']
+PHONE_REGEX = r'^\+?\d{7,15}$'
