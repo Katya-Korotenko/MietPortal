dff0f17f8e28be65b90c8b74f2ddfeb66c421c37
Katya-Korotenko <korotenkok99943@gmail.com>
2026-09-13 12:01:38 +0200

    UPD: validator for phone_number, USERNAME_FIELD, REQUIRED_FIELDS and in Meta ordering

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,16 +1,27 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
 from core.models import UniqueID
-from core.constants import LANDLORD_GROUP, TENANT_GROUP
+from core.constants import LANDLORD_GROUP, TENANT_GROUP, PHONE_REGEX
 
 
 class UserBasic(UniqueID, AbstractUser):
-    phone_number = models.CharField(max_length=20, blank=True)
+    phone_number = models.CharField(max_length=20,
+                                    blank=True,
+                                    validators=[RegexValidator(
+                                        regex=PHONE_REGEX,
+                                        message='Enter a valid phone number.')],
+                                    )
     email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+        ordering = ['username']
 
     def __str__(self):
         return self.username
