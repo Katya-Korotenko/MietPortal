494438993755d835a35056a66a56f741448a9ea1
Katya-Korotenko <korotenkok99943@gmail.com>
2026-09-14 09:45:02 +0200

    added urls

--- apps/statistic/urls.py
+++ apps/statistic/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
+from .views import StatisticsViewSet
 
+router = DefaultRouter()
+router.register('', StatisticsViewSet, basename='statistic')
 
 urlpatterns = [
     path('', include(router.urls)),
