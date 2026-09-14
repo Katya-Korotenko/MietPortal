67210ee59374e2b29fce3f4ea2eb1fbedc3cf295
Katya-Korotenko <korotenkok99943@gmail.com>
2026-09-14 09:48:01 +0200

    added ReviewViewSet

--- apps/reviews/views.py
+++ apps/reviews/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .models import Review
+from .serializers import ReviewSerializer
+
+from .permissions import IsReviewOwner
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    http_method_names = ['get', 'post', 'delete', 'head', 'options']
+
+    def get_queryset(self):
+        queryset = Review.objects.all()
+        listing_id = self.request.query_params.get('listing')
+        if listing_id:
+            queryset = queryset.filter(booking__listing_id=listing_id)
+        return queryset
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [permissions.IsAuthenticated()]
+        if self.action == 'destroy':
+            return [permissions.IsAuthenticated(), IsReviewOwner()]
+        return [permissions.AllowAny()]
